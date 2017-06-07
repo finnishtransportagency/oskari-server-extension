748d2abe7109774274c7063f979456bbe70dc0cc
Marko Kuosmanen <marko.kuosmanen@dimenteq.fi>
2017-06-07 15:15:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# oskari-server-extension for Finnish Transport Agency
+This is a template for extending oskari-server functionality using Maven
---
+++ app-resources/src/main/resources/flyway/livi/V1_0_2__update_mml_basemap_crediantials.sql
@@ -0,0 +1,4 @@
+UPDATE oskari_maplayer
+SET username= 'liikennevirwmtsraja',
+  password='"WMStuotanto1'
+WHERE url='https://karttakuva.maanmittauslaitos.fi/maasto/wmts';
