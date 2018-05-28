f58f1f0aaacc0d15f3c3d3f602f43303605f3ade
ollijak <ollijak@WSLT38.dimenteq.ad>
2018-05-28 10:51:26 +0300

    removed pointless userguide conf

--- app-resources/src/main/resources/flyway/livi/V1_0_1__update_userguide_to_include_lang.sql
+++
@@ -1,7 +0,0 @@
-UPDATE portti_view_bundle_seq 
-SET config='{
-    "tags" : "userguide",
-    "flyoutClazz" : "Oskari.mapframework.bundle.userguide.SimpleFlyout",
-    "includeLang" : true
-}'
-WHERE bundleinstance='userguide'
