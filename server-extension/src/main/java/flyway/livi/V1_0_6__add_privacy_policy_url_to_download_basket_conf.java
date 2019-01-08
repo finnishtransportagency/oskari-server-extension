d4c70f6c535c7883b536302e763332c75c9a7336
Marko Kuosmanen <marko.kuosmanen@sitowise.com>
2019-01-08 15:41:56 +0200

    FLyway adds privacy policy url to download-basket conf

---
+++ server-extension/src/main/java/flyway/livi/V1_0_6__add_privacy_policy_url_to_download_basket_conf.java
@@ -0,0 +1,90 @@
+package flyway.livi;
+
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.util.JSONHelper;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+import org.json.JSONObject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+/**
+ * Adds privacyPolicyUrl to download-basket conf
+ */
+public class V1_0_6__add_privacy_policy_url_to_download_basket_conf implements JdbcMigration {
+    private static final Logger LOG = LogFactory.getLogger(V1_0_6__add_privacy_policy_url_to_download_basket_conf.class);
+
+    public void migrate(Connection connection)
+            throws Exception {
+
+        final ArrayList<Bundle> downloadBasketBundles = getDownloadBasketBundles(connection);
+        for(Bundle bundle: downloadBasketBundles) {
+            if(!modifyConfig(bundle)) {
+                continue;
+            }
+            // update view back to db
+            updateBundleInView(connection, bundle);
+        }
+
+    }
+
+    public static Bundle updateBundleInView(Connection connection, Bundle bundle)
+            throws SQLException {
+        final String sql = "UPDATE portti_view_bundle_seq SET " +
+                "config=? " +
+                " WHERE bundle_id=? " +
+                " AND view_id=?";
+
+        try (final PreparedStatement statement =
+                     connection.prepareStatement(sql)) {
+            statement.setString(1, bundle.config);
+            statement.setLong(2, bundle.bundleId);
+            statement.setLong(3, bundle.viewId);
+            statement.execute();
+        }
+        return null;
+    }
+
+    private boolean modifyConfig(Bundle bundle) throws Exception {
+        JSONObject config = JSONHelper.createJSONObject(bundle.config);
+        if(config == null) {
+            LOG.warn("Couldn't get config JSON for view:", bundle.viewId);
+            return false;
+        }
+        config.remove("privacyPolicyUrl");
+        config.put("privacyPolicyUrl", "https://vayla.fi/yhteystiedot/tietosuoja");
+        bundle.config = config.toString(2);
+        return true;
+    }
+
+    private ArrayList<Bundle> getDownloadBasketBundles(Connection connection) throws Exception {
+        ArrayList<Bundle> ids = new ArrayList<>();
+        final String sql = "SELECT view_id, bundle_id, config FROM portti_view_bundle_seq " +
+                "WHERE bundle_id = (SELECT id FROM portti_bundle WHERE name = 'download-basket')";
+        try (final PreparedStatement statement =
+                     connection.prepareStatement(sql);
+             ResultSet rs = statement.executeQuery()) {
+            while(rs.next()) {
+                Bundle b = new Bundle();
+                b.viewId = rs.getLong("view_id");
+                b.bundleId = rs.getLong("bundle_id");
+                b.config = rs.getString("config");
+                ids.add(b);
+            }
+        }
+        return ids;
+    }
+
+}
+
+
+class Bundle {
+    long viewId;
+    long bundleId;
+    String config;
+}
