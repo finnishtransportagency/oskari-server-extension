f42c59425d43bc92fba7db18d351a487c7a4df06
osqq <osqq95@gmail.com>
2020-08-18 17:58:59 +0300

    added missing flyway

---
+++ server-extension/src/main/flyway/livi/V1_0_1__update_userguide_to_include_lang.sql
@@ -0,0 +1,7 @@
+UPDATE portti_view_bundle_seq 
+SET config='{
+    "tags" : "userguide",
+    "flyoutClazz" : "Oskari.mapframework.bundle.userguide.SimpleFlyout",
+    "includeLang" : true
+}'
+WHERE bundleinstance='userguide'
